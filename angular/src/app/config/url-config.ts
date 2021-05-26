3c026dc64df1409636e1b93021ae5fbac067df9c
Deepak <deepaksharmawins@gmail.com>
2021-05-26 10:36:35 +0530

    First Git Push

---
+++ angular/src/app/config/url-config.ts
@@ -0,0 +1,8 @@
+export const BaseUrl = {
+  // baseApiUrl: "https://www.aayhs.app/api/",
+  // filesUrl:"https://www.aayhs.app"
+
+  baseApiUrl: "https://localhost:44386/api/",
+  filesUrl:"https://localhost:44386"
+
+}
---
+++ angular/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  baseApiUrl: "https://www.aayhs.app/api/",
+  filesUrl:"https://www.aayhs.app"
+};
+
