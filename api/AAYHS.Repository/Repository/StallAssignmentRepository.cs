dd7fa22255c1e6663d72c2d646cbe07b97d22610
Deepak <deepaksharmawins@gmail.com>
2021-05-27 09:35:29 +0530

    added stall assignment

--- api/AAYHS.Repository/Repository/StallAssignmentRepository.cs
+++ api/AAYHS.Repository/Repository/StallAssignmentRepository.cs
@@ -56,6 +56,34 @@ namespace AAYHS.Repository.Repository
             return getAllStall;
         }
 
+        public GetAllStall GetAllUnAssignedStalls()
+        {
+            GetAllStall getAllStall = new GetAllStall();
+            var stall = (from stallAssign in _ObjContext.StallAssignment
+                         where stallAssign.IsActive == true && stallAssign.IsDeleted == false
+                         select new StallResponse
+                         {
+                             StallAssignmentId = stallAssign.StallAssignmentId,
+                             StallId = stallAssign.StallId,
+                             StallAssignmentTypeId = stallAssign.StallAssignmentTypeId,
+                             GroupId = stallAssign.GroupId,
+                             ExhibitorId = stallAssign.ExhibitorId,
+                             BookedByType = stallAssign.BookedByType,
+                             BookedByName = stallAssign.BookedByType == "Group" ?
+                             (from grp in _ObjContext.Groups
+                              where grp.GroupId == stallAssign.GroupId
+                              select grp.GroupName).FirstOrDefault() :
+                             (from exb in _ObjContext.Exhibitors
+                              where exb.ExhibitorId == stallAssign.ExhibitorId
+                              select exb.FirstName + ' ' + exb.LastName).FirstOrDefault()
+
+                         }).ToList();
+
+            getAllStall.stallResponses = stall.ToList();
+            getAllStall.TotalRecords = stall.Count();
+            return getAllStall;
+        }
+
         public MainResponse RemoveAllGroupAssignedStalls(int GroupId)
         {
             
