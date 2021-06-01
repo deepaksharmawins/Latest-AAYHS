59a1373f0f6828cfe783a45be6ecb56839fb40fe
deepak <deepaksharmawins@gmail.com>
2021-06-01 16:44:58 +0530

    added changes in environment

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseApiUrl: "https://www.aayhs.app/api/",
-  filesUrl:"https://www.aayhs.app"
+  baseApiUrl: "http://92.204.135.120:8094/api/",
+  filesUrl:"http://92.204.135.120:8094"
 };
 
